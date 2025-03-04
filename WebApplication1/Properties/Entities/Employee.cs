07fd910d66da26bbf13452244c25d6c4354fca2d
rutSimanTov <ruti8288@gmail.com>
2025-03-05 01:18:55 +0200

    Update and rename Employer.cs to Employee.cs

--- WebApplication1/Properties/Entities/Employee.cs
+++ WebApplication1/Properties/Entities/Employee.cs
@@ -1,6 +1,6 @@
 ﻿namespace WebApplication1.Properties.Entities
 {
-    public class Employer
+    public class employee
     {
         public int Id { get; set; }
         public string Name { get; set; }
